ea0f47da325507b8b90f084ae3456256a9e4a7ad
Dong <ssuny303@gmail.com>
2022-03-30 14:10:32 +0900

    notice imsi

--- Backend/model/noticeDAO.js
+++ Backend/model/noticeDAO.js
@@ -1,8 +1,8 @@
 const db = require('../config/dbConn');
 
-const read_notice = () => {
+const read_notice_list = () => {
     return new Promise((resolve, reject) => {
-        db.query(`SELECT * FROM notice`, (err, db_data) => {
+        db.query(`SELECT notice_num, notice_title, createAt, writer, views FROM notice`, (err ,db_data) => {
             if(err) {
                 reject(err);
                 console.log(err);
@@ -13,7 +13,20 @@ const read_notice = () => {
     })
 }
 
-const create_notice = () => {
+const read_notice = (parameters) => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM notice WHERE notice_num = ?`, parameters, (err, db_data) => {
+            if(err) {
+                reject(err);
+                console.log(err);
+            } else {
+                resolve(db_data);
+            }
+        })
+    })
+}
+
+const create_notice = (parameters) => {
     return new Promise((resolve, reject) => {
         db.query(`INSERT INTO notice SET ?`, parameters, (err, db_data) => {
             if(err) {
@@ -26,7 +39,22 @@ const create_notice = () => {
     })
 }
 
+const update_notice = (parameters) => {
+    return new Promise((resolve, reject) => {
+        db.query(`UPDATE notice SET notice_title = ?, notice_content = ? WHERE notice_num = ?`, [parameters.notice_title, parameters.notice_content, parameters.notice_num], (err, db_data) => {
+            if(err) {
+                reject(err);
+                console.log(err);
+            } else {
+                resolve(db_data);
+            }
+        })
+    })
+}
+
 module.exports = {
+    read_notice_list,
     read_notice,
-    create_notice
+    create_notice,
+    update_notice
 }
--- Backend/routes/notice.js
+++ Backend/routes/notice.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const noticeCtrl = require('../controller/noticeCtrl');
 
 //query
-router.get('/notice/list', noticeCtrl.readNotice);
+router.get('/notice/list', noticeCtrl.readNoticeList);
 
 //query, 
-router.get('/notice/view', (req, res) => {
-    console.log(req.query);
-})
+router.get('/notice/view', noticeCtrl.readNotice);
 
-router.post('/notice')
+router.post('/notice/write', noticeCtrl.createNotice);
+
+router.put('/notice/update', noticeCtrl.updateNotice);
+
 module.exports = router;
