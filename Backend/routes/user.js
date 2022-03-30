c84d24e18664c4aa21253793984a44bdec83453a
jiwonman <kusa1230@naver.com>
2022-03-31 00:42:33 +0900

    passport 구조

--- Backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  console.log('root');
-  res.sendStatus(200);
-})
-
-module.exports = router;
