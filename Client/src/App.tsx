050d9ad594b08b2be7d5ef81faee4d1031a91dca
chanyoung <chanyoung2371@naver.com>
2022-04-03 23:21:48 +0900

    test save

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import About from './Page/About'
+import About_list from './Component/About/About_list'
+import styled from 'styled-components'
+import imgA from './images/main_1.png'
+import './App.css';
+
+const Layout = styled.img`
+  width: 100vw;
+  min-height: 100vh;
+  background-image: url(${imgA}) ;
+  border: 1px solid red;
+`;
+const Button = styled.button`
+width: 200px;
+height:60px;
+background-color:skyblue;
+`
 
 function App() {
   return (
-    <div>
-      <About/>
+
+    <div className="App">
+      <About_list />
+      <header className="App-header">
+        <img src={imgA} className="App-logo" alt="logo" />
+        <p>
+          App Download
+        </p>
+       <Button>APP</Button>
+      </header>
     </div>
   )
 }
