da9b073222af708574c7a965b33c5a21f3face5f
chanyoung <chanyoung2371@naver.com>
2022-04-04 17:27:46 +0900

    add css animation

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import About_list from './Component/About/About_list'
 import styled from 'styled-components'
 import imgA from './images/main_1.png'
+import imgB from './images/download.png'
 import './App.css';
 
-const Layout = styled.img`
-  width: 100vw;
-  min-height: 100vh;
-  background-image: url(${imgA}) ;
-  border: 1px solid red;
-`;
-const Button = styled.button`
+// const Layout = styled.img`
+//   width: 100vw;
+//   min-height: 100vh;
+//   background-image: url(${imgA}) ;
+//   border: 1px solid red;
+// `;
+const Button = styled.img`
 width: 200px;
 height:60px;
-background-color:skyblue;
 `
 
 function App() {
@@ -23,10 +23,17 @@ function App() {
       <About_list />
       <header className="App-header">
         <img src={imgA} className="App-logo" alt="logo" />
+        <div className='font_ani'>
+          <span>B</span>
+          <span>U</span>
+          <span>B</span>
+          <span>B</span>
+          <span>A</span>
+        </div>
         <p>
-          App Download
+          {/* <Button src={imgB}></Button> */}
         </p>
-       <Button>APP</Button>
+
       </header>
     </div>
   )
