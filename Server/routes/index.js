fa7daf1fa49d5c4362cd3a24f2ccb1aa50a13c89
Dong <ssuny303@gmail.com>
2022-04-06 17:27:26 +0900

    asd

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   console.log('dong');
-  res.send(`<a href="http://localhost:3000/auth">auth</a><br><a href="http://localhost:3000/baby">baby</a>`);
+  // res.send(`<a href="http://localhost:3000/auth">auth</a><br><a href="http://localhost:3000/baby">baby</a>`);
+  res.send(`<a href="http://localhost:8000/auth">auth</a><br><a href="http://localhost:8000/baby">baby</a>`);
 })
 
 module.exports = router;
