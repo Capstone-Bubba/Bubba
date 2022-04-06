d2b110735c1d0bc87a8adb77587b364bd6fd8ffe
jiwonman <kusa1230@naver.com>
2022-04-07 01:52:01 +0900

    session에 user_num 저장, authCheck 만들었으나 db의 tinyint(1) 값 가져오지 못함

--- Server/routes/notice.js
+++ Server/routes/notice.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const noticeCtrl = require('../controller/noticeCtrl');
+const auth = require('../middleware/sessoinCheck');
 
 //query
 router.get('/notice/list', noticeCtrl.readNoticeList);
@@ -8,8 +9,8 @@ router.get('/notice/list', noticeCtrl.readNoticeList);
 //query, 
 router.get('/notice/view', noticeCtrl.readNotice);
 
-router.post('/notice/write', noticeCtrl.createNotice);
+router.post('/notice/write', auth.authorityCheck, noticeCtrl.createNotice);
 
-router.put('/notice/update', noticeCtrl.updateNotice);
+router.put('/notice/update', auth.authorityCheck, noticeCtrl.updateNotice);
 
 module.exports = router;
