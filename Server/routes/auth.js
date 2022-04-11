a95e6ef727be8c504f2db37ffd8a41d265ca7c2e
Dong <ssuny303@gmail.com>
2022-04-11 22:14:56 +0900

    login

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -7,13 +7,19 @@ const auth = require('../middleware/sessoinCheck');
 router.get('/', authCtrl.selectLogin)
 
 router.get('/naver', auth.sessionCheck, passport.authenticate('naver'));
-router.get('/naver/callback', passport.authenticate('naver', { failureRedirect: "/auth/naver", successRedirect: "/" }));
+router.get('/naver/callback', passport.authenticate('naver', { failureRedirect: "/auth/naver" }), async (req, res) => {
+    return res.status(200).redirect('http://localhost:3001');
+});
 
-router.get('/google', auth.sessionCheck, passport.authenticate('google', { scope: ['email'] }));
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/auth/google', successRedirect: "/" }));
+router.get('/google',auth.sessionCheck, passport.authenticate('google', { scope: ['email'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://localhost:3000/auth/google'}), async (req, res) => {
+    return res.status(200).redirect('http://localhost:3001');
+});
 
 router.get('/kakao', auth.sessionCheck, passport.authenticate('kakao'));
-router.get('/kakao/callback', passport.authenticate('kakao', { failureRedirect: "/auth/kakao", successRedirect: "/" }));
+router.get('/kakao/callback', passport.authenticate('kakao', { failureRedirect: "/auth/kakao" }), async (req, res) => {
+    return res.status(200).redirect('http://localhost:3001');
+});
 
 router.get('/logout', authCtrl.logout);
 
