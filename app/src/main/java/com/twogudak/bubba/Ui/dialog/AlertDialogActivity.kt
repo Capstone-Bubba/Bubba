a789ebd16b4f367bac0799bc2bbfefaf71c9bb8c
Twogudak <sunsu9944@naver.com>
2022-10-19 04:48:37 +0900

    FIX

--- app/src/main/java/com/twogudak/bubba/Ui/dialog/AlertDialogActivity.kt
+++ app/src/main/java/com/twogudak/bubba/Ui/dialog/AlertDialogActivity.kt
@@ -19,7 +19,7 @@ import org.w3c.dom.Text
 import java.net.URI
 import java.util.*
 
-class AlertDialogActivity() : AppCompatActivity(), TextToSpeech.OnInitListener {
+class AlertDialogActivity() : AppCompatActivity() {
 
     lateinit var AlertDialogContent: TextView
     lateinit var AlertDialogCheckButton: Button
@@ -33,20 +33,6 @@ class AlertDialogActivity() : AppCompatActivity(), TextToSpeech.OnInitListener {
     private lateinit var Ring: Ringtone
     private var tts : TextToSpeech? = null
 
-    override fun onInit(p0: Int) {
-        tts = TextToSpeech(this, TextToSpeech.OnInitListener {
-            if (it == TextToSpeech.SUCCESS) {
-                val result = tts!!.setLanguage(Locale.KOREAN)
-                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                    Toast.makeText(this, "TTS Not Suppoerted", Toast.LENGTH_SHORT).show()
-                    return@OnInitListener
-                }
-            } else {
-                Toast.makeText(this, "TTS Init failed", Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,17 +44,17 @@ class AlertDialogActivity() : AppCompatActivity(), TextToSpeech.OnInitListener {
         Log.d("AlertDialogActivity", contentText.toString())
 
 
-//        val RingURL = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-//        Ring = RingtoneManager.getRingtone(applicationContext,RingURL)
-//        Ring.play()
+        val RingURL = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        Ring = RingtoneManager.getRingtone(applicationContext,RingURL)
+
 
         AlertDialogContent = findViewById(R.id.AlertDialog_context_text)
         AlertDialogCheckButton = findViewById(R.id.AlertDialog_Check_Button)
 
         if (contentText != "null") {
             VibratorSetting(contentText!!)
-            //initTextToSpech()
-            SpeackTTS(contentText)
+            initTextToSpech(contentText!!)
+
             AlertDialogContent.text = contentText
         } else {
             AlertDialogContent.text = ""
@@ -78,11 +64,15 @@ class AlertDialogActivity() : AppCompatActivity(), TextToSpeech.OnInitListener {
 
 
         AlertDialogCheckButton.setOnClickListener {
+            Ring.stop()
             if (contentText != "null") {
                 vibrator.cancel()
-                tts?.stop()
+                SpeackTTS(contentText){
+                    finish()
+                }
             }
-            finish()
+
+
         }
 
     }
@@ -114,13 +104,17 @@ class AlertDialogActivity() : AppCompatActivity(), TextToSpeech.OnInitListener {
         return vibrator
     }
 
-    private fun initTextToSpech() {
+    private fun initTextToSpech(Content: String) {
         tts = TextToSpeech(this, TextToSpeech.OnInitListener {
             if (it == TextToSpeech.SUCCESS) {
                 val result = tts!!.setLanguage(Locale.KOREAN)
                 if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                     Toast.makeText(this, "TTS Not Suppoerted", Toast.LENGTH_SHORT).show()
                     return@OnInitListener
+                } else {
+                    SpeackTTS(Content){
+                        Ring.play()
+                    }
                 }
             } else {
                 Toast.makeText(this, "TTS Init failed", Toast.LENGTH_SHORT).show()
@@ -128,32 +122,39 @@ class AlertDialogActivity() : AppCompatActivity(), TextToSpeech.OnInitListener {
         })
     }
 
-    private fun SpeackTTS(Content: String) {
+    private fun SpeackTTS(Content: String, callback: () -> Unit) {
         when (Content) {
             "뒷면" -> {
                 tts?.speak("아이가 엎드려 있습니다", TextToSpeech.QUEUE_FLUSH, null,null)
-                tts?.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null)
-                tts?.engines
+                tts?.playSilentUtterance(500, TextToSpeech.QUEUE_ADD, null)
+                tts?.speak("아이가 엎드려 있습니다", TextToSpeech.QUEUE_FLUSH, null,null)
+                callback.invoke()
             }
             "불편함" -> {
-                tts?.speak("아이가 불편하여 울고있습니다.", TextToSpeech.QUEUE_ADD, null, null)
-                tts?.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null)
+                tts?.speak("아이가 불편하여 울고있습니다.", TextToSpeech.QUEUE_FLUSH, null, null)
+                tts?.playSilentUtterance(500, TextToSpeech.QUEUE_ADD, null)
+                tts?.speak("아이가 불편하여 울고있습니다.", TextToSpeech.QUEUE_FLUSH, null, null)
+                callback.invoke()
             }
             "배아픔" -> {
-                tts?.speak("아이가 배가 아파 울고있습니다.", TextToSpeech.QUEUE_ADD, null, null)
-                tts?.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null)
+                tts?.speak("아이가 배가 아파 울고있습니다.", TextToSpeech.QUEUE_FLUSH, null, null)
+                tts?.playSilentUtterance(500, TextToSpeech.QUEUE_ADD, null)
+                callback.invoke()
             }
             "배고픔" -> {
-                tts?.speak("아이가 배고파 울고 있습니다", TextToSpeech.QUEUE_ADD, null, null)
+                tts?.speak("아이가 배고파 울고 있습니다", TextToSpeech.QUEUE_FLUSH, null, null)
                 tts?.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null)
+                callback.invoke()
             }
             "트름" -> {
-                tts?.speak("아이가 트름을 하고 싶어 울고있습니다.", TextToSpeech.QUEUE_ADD, null, null)
+                tts?.speak("아이가 트름을 하고 싶어 울고있습니다.", TextToSpeech.QUEUE_FLUSH, null, null)
                 tts?.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null)
+                callback.invoke()
             }
             "피곤함" -> {
-                tts?.speak("아이가 피곤하여 울고있습니다.", TextToSpeech.QUEUE_ADD, null, null)
+                tts?.speak("아이가 피곤하여 울고있습니다.", TextToSpeech.QUEUE_FLUSH, null, null)
                 tts?.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null)
+                callback.invoke()
             }
         }
     }
